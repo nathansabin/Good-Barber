94ed1f141465195ead55d24eba28ce429227e265
nathansabin <sabinnat001@gmail.com>
2023-12-14 20:58:09 -0800

    set up footer

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Navigation from '../navigation';
+import './header.css'
+//import goodbarber from '../../assets/images/goodbarber';
 
 function Header() {
     return (
-        <header>
-            
+        <header className='header-format'>
+            <div className='header-content'>
+                <img alt="Good Barber" />
+                <h1>The Good Barber</h1>
+                <Navigation />
+            </div>
         </header>
     );
 }
