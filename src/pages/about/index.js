02714069aea1c3b97c3071cf642e98f2c6975cdb
nathansabin <sabinnat001@gmail.com>
2023-12-27 18:42:25 -0800

    fixed header bug

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -3,8 +3,13 @@ import './about.css';
 
 function About() {
     return (
-        <div>
-            about   
+        <div className='about-container'>
+            <div>
+                <img></img>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tristique nulla aliquet enim tortor. Amet nisl purus in mollis. Lorem mollis aliquam ut porttitor leo. Posuere urna nec tincidunt praesent semper. Et egestas quis ipsum suspendisse ultrices. Fringilla ut morbi tincidunt augue interdum. Tortor vitae purus faucibus ornare suspendisse sed nisi lacus sed. Leo integer malesuada nunc vel risus commodo viverra maecenas. Amet consectetur adipiscing elit duis tristique sollicitudin. Nibh ipsum consequat nisl vel pretium lectus quam. Non consectetur a erat nam at lectus urna duis convallis. Dolor purus non enim praesent. Elit ut aliquam purus sit amet. Nec feugiat nisl pretium fusce id velit ut.
+
+Semper eget duis at tellus at urna. Mauris pharetra et ultrices neque ornare aenean euismod elementum. Vestibulum mattis ullamcorper velit sed. Erat nam at lectus urna duis convallis convallis tellus id. Dignissim cras tincidunt lobortis feugiat vivamus at. Nec sagittis aliquam malesuada bibendum arcu vitae elementum. Lorem ipsum dolor sit amet consectetur adipiscing elit. Amet purus gravida quis blandit. Facilisis gravida neque convallis a cras semper auctor neque. Nulla facilisi cras fermentum odio eu feugiat. Bibendum neque egestas congue quisque egestas. A arcu cursus vitae congue mauris rhoncus. Porta nibh venenatis cras sed felis. Congue mauris rhoncus aenean vel. Lectus arcu bibendum at varius. Praesent tristique magna sit amet purus. Mi sit amet mauris commodo quis imperdiet.</p>   
         </div>
     );
 }
